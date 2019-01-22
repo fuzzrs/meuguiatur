4feedec31c243e184fadd8ba7837f2fc8169e0f6
Leonardo S Santos <fuzzrs@gmail.com>
2019-01-22 19:49:11 -0200

    carousel no rodape e novo banner

--- js/scripts.js
+++ js/scripts.js
@@ -63,6 +63,47 @@ $('.card-carousel').slick({
 });
 });
 
+$(document).ready(function(){
+$('.banner-carousel').slick({
+  dots: false,
+  arrows: false,
+  infinite: false,
+  autoHeight: false,
+  speed: 300,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        dots: false,
+        arrows: true
+      }
+    },
+    {
+      breakpoint: 860,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    },
+    {
+      breakpoint: 600,
+      arrows: false,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
+});
+});
+
 
 // $("document").ready(function($){
 //     var nav = $('.nav-bar');
